f33410b1b10db10e5e123372ef789490bc049220
Mathieu Bridon <mathieu@hashbang.fr>
2019-09-09 13:58:10 +0200

    Start working on the app
    This allows starting a web application written with aiohttp.
    
    The app doesn't do anything yet though.

---
+++ eos_metrics_proxy/__main__.py
@@ -0,0 +1,24 @@
+# Copyright (c) 2019 - Endless
+#
+# This file is part of eos-metrics-proxy
+#
+# eos-metrics-proxy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# eos-metrics-proxy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with eos-metrics-proxy.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from aiohttp.web import run_app
+
+from .app import get_app
+
+
+run_app(get_app())
