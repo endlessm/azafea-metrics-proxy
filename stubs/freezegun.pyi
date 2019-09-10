e0cd494bf83a2c202ff4fbbfb4e0d1310fbe67a0
Mathieu Bridon <mathieu@hashbang.fr>
2019-09-10 12:05:23 +0200

    Add partial stubs for freezegun
    This only contains what will be useful for the tests.

---
+++ stubs/freezegun.pyi
@@ -0,0 +1,35 @@
+# Copyright (c) 2019 - Endless
+#
+# This file is part of eos-metrics-proxy
+#
+# eos-metrics-proxy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# eos-metrics-proxy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with eos-metrics-proxy.  If not, see <http://www.gnu.org/licenses/>.
+
+
+# TODO: Ask upstream whether they want type annotations in their code, or would be open to having
+# stubs in typeshed
+
+
+from datetime import date, timedelta
+from typing import Callable, Generator, List, Optional, Union
+
+
+# https://github.com/spulec/freezegun/blob/master/freezegun/api.py#L495
+class _freeze_time: ...
+
+def freeze_time(time_to_freeze: Optional[Union[str, date, timedelta, Callable, Generator]] = None,
+                tz_offset: Union[int, timedelta] = 0,
+                ignore: List = None,
+                tick: bool = False,
+                as_arg: bool = False,
+                auto_tick_seconds: int = 0) -> _freeze_time: ...
