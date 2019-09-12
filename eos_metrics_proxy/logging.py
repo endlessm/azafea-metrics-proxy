2ff12ab23c5b0d1a5811764da496b4cbacb4525d
Mathieu Bridon <mathieu@hashbang.fr>
2019-09-12 10:13:09 +0200

    Pull in the logging code from Azafea
    Eventually making this a shared module between the two would be nice,
    but for now this will have to do.

---
+++ eos_metrics_proxy/logging.py
@@ -0,0 +1,68 @@
+# Copyright (c) 2019 - Endless
+#
+# This file is part of eos-metrics-proxy
+#
+# eos-metrics-proxy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# eos-metrics-proxy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with eos-metrics-proxy.  If not, see <http://www.gnu.org/licenses/>.
+
+
+# This whole file is almost copy-pasted from Azafea.
+# TODO: Can we share most of this into its own module?
+
+
+import logging
+import sys
+
+
+class StdoutFilter(logging.Filter):
+    def filter(self, record: logging.LogRecord) -> bool:
+        return record.levelno <= logging.INFO
+
+
+def _reset_logging() -> None:
+    # This is necessary for 2 reasons:
+    #
+    # 1.  pytest sets up the logging system (to capture logs) which means when setup_logging is
+    #     called its setup is ignored;
+    # 2.  after calling setup_logging once, calling a second time doesn't do anything either;
+    #
+    # Both issues make it impossible to properly test the setup_logging function without resetting
+    # the log setup.
+    #
+    # As this undoes the logging setup pytest does though, it means we can't use their caplog
+    # fixture. Instead, log messages are sent to their capsys/capfd fixtures.
+    root = logging.getLogger()
+
+    for handler in root.handlers[:]:
+        handler.close()
+        root.removeHandler(handler)
+
+
+def setup_logging(*, verbose: bool = False) -> None:
+    _reset_logging()
+
+    level = logging.DEBUG if verbose else logging.INFO
+
+    # Send DEBUG and INFO to stdout, WARNING and ERROR to stderr
+    out = logging.StreamHandler(stream=sys.stdout)
+    out.setLevel(logging.DEBUG)
+    out.addFilter(StdoutFilter())
+    err = logging.StreamHandler(stream=sys.stderr)
+    err.setLevel(logging.WARNING)
+
+    format_ = '[{levelname}] {name}: {message}'
+    logging.basicConfig(level=level, handlers=[out, err], format=format_, style='{')
+
+    if verbose:
+        # Decrease verbosity
+        logging.getLogger('flake8').setLevel(logging.WARNING)
---
+++ eos_metrics_proxy/tests/test_logging.py
@@ -0,0 +1,74 @@
+# Copyright (c) 2019 - Endless
+#
+# This file is part of eos-metrics-proxy
+#
+# eos-metrics-proxy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# eos-metrics-proxy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with eos-metrics-proxy.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import logging
+
+from eos_metrics_proxy.logging import setup_logging
+
+
+def test_default(capfd):
+    setup_logging(verbose=False)
+    log = logging.getLogger(__name__)
+
+    log.debug('A debugging message')
+    log.info('An informative message')
+    log.warning('A scary warning')
+    log.error('Oh no!')
+
+    capture = capfd.readouterr()
+    assert 'A debugging message' not in capture.out
+    assert 'An informative message' in capture.out
+    assert 'A scary warning' in capture.err
+    assert 'Oh no!' in capture.err
+
+
+def test_verbose(capfd):
+    setup_logging(verbose=True)
+    log = logging.getLogger(__name__)
+
+    log.debug('A debugging message')
+    log.info('An informative message')
+    log.warning('A scary warning')
+    log.error('Oh no!')
+
+    capture = capfd.readouterr()
+    assert 'A debugging message' in capture.out
+    assert 'An informative message' in capture.out
+    assert 'A scary warning' in capture.err
+    assert 'Oh no!' in capture.err
+
+
+def test_reset_logging(capfd):
+    setup_logging(verbose=False)
+    log = logging.getLogger(__name__)
+
+    log.debug('A debugging message')
+    capture = capfd.readouterr()
+    assert 'A debugging message' not in capture.out
+
+    setup_logging(verbose=True)
+
+    log.debug('A debugging message')
+    capture = capfd.readouterr()
+    assert 'A debugging message' in capture.out
+
+    setup_logging(verbose=False)
+
+    log.debug('A debugging message')
+    capture = capfd.readouterr()
+    assert 'A debugging message' not in capture.out
