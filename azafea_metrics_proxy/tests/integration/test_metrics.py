3146e7f73ff28796220c0578e25681beac1d716f
Guillaume Ayoub <guillaume@courtbouillon.org>
2021-05-18 18:26:03 +0200

    Allow version 3 for metrics

--- azafea_metrics_proxy/tests/integration/test_metrics.py
+++ azafea_metrics_proxy/tests/integration/test_metrics.py
@@ -26,11 +26,11 @@ import pytest
 from azafea_metrics_proxy.utils import get_timestamp, utcnow
 
 
-async def test_put_metrics_request(aiohttp_client, app):
+@pytest.mark.parametrize('version', [2, 3])
+async def test_put_metrics_request(aiohttp_client, app, version):
     client = await aiohttp_client(app)
 
     now = utcnow()
-    version = 2
     sha512 = ('3eaab42c4ea4c201a726f44b82e51b52f0a587399a59c32f92b4634b60fef0c5f3fbaec705a8a6d7dad'
               '216d48b3a948ced2be27efb4547ea3bf657d437aec838')
     metrics_request = (b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x945w\x00\x00\x00\x00\x00\xa5E\x04'
@@ -43,15 +43,15 @@ async def test_put_metrics_request(aiohttp_client, app):
     assert response.status == 200
     assert await response.text() == 'OK'
 
-    assert await app['redis'].llen('metrics-2') == 1
-    assert await app['redis'].rpop('metrics-2') == get_timestamp(now) + metrics_request
+    assert await app['redis'].llen(f'metrics-{version}') == 1
+    assert await app['redis'].rpop(f'metrics-{version}') == get_timestamp(now) + metrics_request
 
 
-async def test_put_gzipped_metrics_request(aiohttp_client, app):
+@pytest.mark.parametrize('version', [2, 3])
+async def test_put_gzipped_metrics_request(aiohttp_client, app, version):
     client = await aiohttp_client(app)
 
     now = utcnow()
-    version = 2
     sha512 = ('3eaab42c4ea4c201a726f44b82e51b52f0a587399a59c32f92b4634b60fef0c5f3fbaec705a8a6d7dad'
               '216d48b3a948ced2be27efb4547ea3bf657d437aec838')
     metrics_request = (b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x945w\x00\x00\x00\x00\x00\xa5E\x04'
@@ -67,8 +67,8 @@ async def test_put_gzipped_metrics_request(aiohttp_client, app):
     assert response.status == 200
     assert await response.text() == 'OK'
 
-    assert await app['redis'].llen('metrics-2') == 1
-    assert await app['redis'].rpop('metrics-2') == get_timestamp(now) + metrics_request
+    assert await app['redis'].llen(f'metrics-{version}') == 1
+    assert await app['redis'].rpop(f'metrics-{version}') == get_timestamp(now) + metrics_request
 
 
 @pytest.mark.parametrize('version', [0, 1])
@@ -94,11 +94,11 @@ async def test_put_metrics_request_old_version(aiohttp_client, app, version):
     assert await app['redis'].llen(f'metrics-{version}') == 0
 
 
-async def test_put_compressed_metrics_request_unknown_compression(aiohttp_client, app):
+@pytest.mark.parametrize('version', [2, 3])
+async def test_put_compressed_metrics_request_unknown_compression(aiohttp_client, app, version):
     client = await aiohttp_client(app)
 
     now = utcnow()
-    version = 2
     sha512 = ('3eaab42c4ea4c201a726f44b82e51b52f0a587399a59c32f92b4634b60fef0c5f3fbaec705a8a6d7dad'
               '216d48b3a948ced2be27efb4547ea3bf657d437aec838')
     metrics_request = (b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x945w\x00\x00\x00\x00\x00\xa5E\x04'
@@ -114,13 +114,13 @@ async def test_put_compressed_metrics_request_unknown_compression(aiohttp_client
     assert response.status == 400
     assert await response.text() == 'Unknown request encoding: bzip2'
 
-    assert await app['redis'].llen('metrics-2') == 0
+    assert await app['redis'].llen(f'metrics-{version}') == 0
 
 
-async def test_put_metrics_request_empty_body(aiohttp_client, app):
+@pytest.mark.parametrize('version', [2, 3])
+async def test_put_metrics_request_empty_body(aiohttp_client, app, version):
     client = await aiohttp_client(app)
 
-    version = 2
     sha512 = ('3eaab42c4ea4c201a726f44b82e51b52f0a587399a59c32f92b4634b60fef0c5f3fbaec705a8a6d7dad'
               '216d48b3a948ced2be27efb4547ea3bf657d437aec838')
     metrics_request = b''
@@ -129,13 +129,13 @@ async def test_put_metrics_request_empty_body(aiohttp_client, app):
     assert response.status == 400
     assert await response.text() == 'Invalid request: empty body'
 
-    assert await app['redis'].llen('metrics-2') == 0
+    assert await app['redis'].llen(f'metrics-{version}') == 0
 
 
-async def test_put_metrics_request_invalid_hash(aiohttp_client, app):
+@pytest.mark.parametrize('version', [2, 3])
+async def test_put_metrics_request_invalid_hash(aiohttp_client, app, version):
     client = await aiohttp_client(app)
 
-    version = 2
     bad_sha512 = ('0000000000000000000000000000000000000000000000000000000000000000000000000000000'
                   '0000000000000000000000000000000000000000000000000')
     sha512 = ('3eaab42c4ea4c201a726f44b82e51b52f0a587399a59c32f92b4634b60fef0c5f3fbaec705a8a6d7dad'
@@ -148,4 +148,4 @@ async def test_put_metrics_request_invalid_hash(aiohttp_client, app):
     assert response.status == 400
     assert await response.text() == f'SHA512 mismatch: expected {bad_sha512} but got {sha512}'
 
-    assert await app['redis'].llen('metrics-2') == 0
+    assert await app['redis'].llen(f'metrics-{version}') == 0
